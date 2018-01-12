d6dd66e8ec85fbe48372b80c1350e46f5472cdc6
Shawn Hubbart <shawn.hubbart@gmail.com>
2018-01-12 15:18:22 -0800

    Added code to open and close the different spell books. Color coded the menu background of each spell book

--- Assets/Scripts/Player/PauseMenu.cs
+++ Assets/Scripts/Player/PauseMenu.cs
@@ -6,7 +6,15 @@ using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject menu;
-    [SerializeField] GameObject spellBook;
+    [SerializeField] GameObject spellbook;
+    [SerializeField] GameObject earthbook;
+    [SerializeField] GameObject firebook;
+    [SerializeField] GameObject frostbook;
+    [SerializeField] GameObject healingbook;
+    [SerializeField] GameObject lightningbook;
+    [SerializeField] GameObject necromancybook;
+    [SerializeField] GameObject shadowbook;
+    [SerializeField] GameObject summoningbook;
     [SerializeField] GameObject hotkeys;
     public Texture2D defaultCursor;
     bool menuOpen;
@@ -77,15 +85,111 @@ public class PauseMenu : MonoBehaviour
     public void OpenSpellbookMenu()
     {
         otherMenusOpen = true;
-        spellBook.SetActive(true);
+        spellbook.SetActive(true);
         menu.SetActive(false);
     }
 
     public void CloseSpellbookMenu()
     {
         otherMenusOpen = false;
-        spellBook.SetActive(false);
+        spellbook.SetActive(false);
         menu.SetActive(true);
+    }
+
+    public void OpenEarthbook()
+    {
+        earthbook.SetActive(true);
+        spellbook.SetActive(false);
+    }
+
+    public void CloseEarthbook()
+    {
+        earthbook.SetActive(false);
+        spellbook.SetActive(true);
+    }
+
+    public void OpenFirebook()
+    {
+        firebook.SetActive(true);
+        spellbook.SetActive(false);
+    }
+
+    public void CloseFirebook()
+    {
+        firebook.SetActive(false);
+        spellbook.SetActive(true);
+    }
+
+    public void OpenFrostbook()
+    {
+        frostbook.SetActive(true);
+        spellbook.SetActive(false);
+    }
+
+    public void CloseFrostbook()
+    {
+        frostbook.SetActive(false);
+        spellbook.SetActive(true);
+    }
+
+    public void OpenHealingbook()
+    {
+        healingbook.SetActive(true);
+        spellbook.SetActive(false);
+    }
+
+    public void CloseHealingbook()
+    {
+        healingbook.SetActive(false);
+        spellbook.SetActive(true);
+    }
+
+    public void OpenLightningbook()
+    {
+        lightningbook.SetActive(true);
+        spellbook.SetActive(false);
+    }
+
+    public void CloseLightningbook()
+    {
+        lightningbook.SetActive(false);
+        spellbook.SetActive(true);
+    }
+
+    public void OpenNecromancybook()
+    {
+        necromancybook.SetActive(true);
+        spellbook.SetActive(false);
+    }
+
+    public void CloseNecromancybook()
+    {
+        necromancybook.SetActive(false);
+        spellbook.SetActive(true);
+    }
+
+    public void OpenShadowbook()
+    {
+        shadowbook.SetActive(true);
+        spellbook.SetActive(false);
+    }
+
+    public void CloseShadowbook()
+    {
+        shadowbook.SetActive(false);
+        spellbook.SetActive(true);
+    }
+
+    public void OpenSummoningbook()
+    {
+        summoningbook.SetActive(true);
+        spellbook.SetActive(false);
+    }
+
+    public void CloseSummoningbook()
+    {
+        summoningbook.SetActive(false);
+        spellbook.SetActive(true);
     }
 
 }

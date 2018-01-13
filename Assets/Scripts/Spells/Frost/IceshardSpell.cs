0b38103f53adeb6e7d4a4fdf016d90c348b1fca1
Shawn Hubbart <shawn.hubbart@gmail.com>
2018-01-13 00:53:36 -0800

    Started work on spellbook layouts and added temp icons to Fireball and Iceshard

--- Assets/Scripts/Spells/Frost/IceshardSpell.cs
+++ Assets/Scripts/Spells/Frost/IceshardSpell.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class FrostboltSpell : MonoBehaviour
+public class IceshardSpell : MonoBehaviour
 {
     [SerializeField] float baseDamage;
     [SerializeField] float duration;

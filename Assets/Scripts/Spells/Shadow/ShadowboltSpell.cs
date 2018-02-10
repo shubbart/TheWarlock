4e66c6766c163624bfac8d0344b4344860961baf
Shawn Hubbart <s179128@students.aie.edu.au>
2018-02-10 15:04:54 -0800

    Added new Shadow spell: Shadowbolt. Still needs an icon

---
+++ Assets/Scripts/Spells/Shadow/ShadowboltSpell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowboltSpell : MonoBehaviour
+{
+    [SerializeField] float baseDamage;
+    [SerializeField] float duration;
+    float spellPower;
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        spellPower = player.GetComponent<PlayerCharacterSheet>().player.spellPower;
+    }
+
+    private void Update()
+    {
+        duration -= Time.deltaTime;
+
+        if (duration <= 0)
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemySheet>().TakeDamage(baseDamage + spellPower);
+            Debug.Log("Enemy remaining HP: " + collision.gameObject.GetComponent<EnemySheet>().enemy.currentHealth);
+            Destroy(gameObject);
+        }
+    }
+}
